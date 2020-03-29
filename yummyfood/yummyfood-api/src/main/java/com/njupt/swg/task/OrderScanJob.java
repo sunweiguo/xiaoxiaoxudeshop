75ea814c0fa03bc6faed3e6e6ba10fa56406bf98
sunweiguo <317758022@qq.com>
2020-03-29 20:48:58 +0800

    提交人：fossi 提交内容： 1、定时任务的弊端说明

--- yummyfood/yummyfood-api/src/main/java/com/njupt/swg/task/OrderScanJob.java
+++ yummyfood/yummyfood-api/src/main/java/com/njupt/swg/task/OrderScanJob.java
@@ -18,6 +18,13 @@ public class OrderScanJob {
     @Autowired
     private IOrderService orderService;
 
+    /**
+     * 定时任务存在佷多的弊端，比较明显的弊端是：
+     *  这里我们用的这个sprong scheduled不支持集群，或者说需要其他的条件支撑才能在集群条件下跑，否则会出现多个服务器同时关闭相同订单的现象
+     *  当然也可以单独抽出这个定时任务来执行，没有集群环境下的困扰。
+     *  但是第二个弊端是：效率低。全表扫描极大浪费mysql的查询性能。导致其他业务阻塞。所以定时扫描这种在稍微大一点的体量时就不适用了。
+     *  解决方案：用消息队列来做一个延时队列出来处理。后续将会进行改进。
+     */
     @Scheduled(cron = "0/10 * * * * ?")
     public void autoCloseOrder(){
         log.info("开始执行定时任务，定时修改超时一天的订单状态为关闭");
