48a1feaec003022d9b6781f51606cbecd8d3f389
sunweiguo <317758022@qq.com>
2020-03-29 15:43:27 +0800

    提交人：fossi 提交内容： 1、收货地址相关逻辑

--- yummyfood/yummyfood-api/src/main/java/com/njupt/swg/controller/UserController.java
+++ yummyfood/yummyfood-api/src/main/java/com/njupt/swg/controller/UserController.java
@@ -125,6 +125,8 @@ public class UserController {
                                    HttpServletRequest request,
                                    HttpServletResponse response){
         CookieUtils.deleteCookie(request,response,"user");
+        //删除购物车的cookie
+        CookieUtils.deleteCookie(request,response,"shopcart");
 //        TODO 1、清空购物车
 //        TODO 2、清除分布式会话中的用户数据
         return CommonJsonResult.ok();
---
+++ yummyfood/yummyfood-service/src/main/java/com/njupt/swg/service/IAddressService.java
@@ -0,0 +1,23 @@
+package com.njupt.swg.service;
+
+import com.njupt.swg.bo.AddressNewAddBO;
+import com.njupt.swg.pojo.UserAddress;
+import com.njupt.swg.utils.CommonJsonResult;
+
+import java.util.List;
+
+/**
+ * @Author swg.
+ * @Date 2020/3/28 23:20
+ * @CONTACT 317758022@qq.com
+ * @DESC
+ */
+public interface IAddressService {
+    List<UserAddress> getAllUserAddressedByUserId(String userId);
+
+    CommonJsonResult createNewAddress(AddressNewAddBO addressNewAddBO);
+
+    CommonJsonResult deleteAddress(String userId, String addressId);
+
+    CommonJsonResult setDefalutAddress(String userId, String addressId);
+}
